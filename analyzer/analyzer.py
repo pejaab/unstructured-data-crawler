0d0eaeffec3402e4ef9e7a997d979adc1a6155b7
Peter Schaab <pejaab@gmail.com>
2015-06-30 11:29:07 +0200

    makes search case insensitive

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,15 +1,19 @@
 import lxml.html as lh
 from lxml.etree import tostring
+from lxml import etree
+import urllib.request
 from dao import Dao
 
 class Analyzer(object) :
-
+   #TODO: maybe initialize the analyzer with the url so the read, lower, encode cycle does not have to be done for every input
    xpath = "//*[contains(normalize-space(.), '{0}') and not(.//*[contains(normalize-space(.), '{0}')])]/*"
 
    @classmethod
    def search_path(self, url, input_string) :
-       elem_tree = lh.parse(url)
-       node = elem_tree.xpath(self.xpath.format(input_string))[0]
+       html = etree.HTML(urllib.request.urlopen(url).read().decode('utf-8').lower())
+       #elem_tree = lh.parse(url)
+       elem_tree = etree.ElementTree(html)
+       node = elem_tree.xpath(self.xpath.format(input_string.lower()))[0]
        path = elem_tree.getpath(node)
        #Use parent path
        path = path[:path.rfind('/')]
@@ -24,7 +28,8 @@ class Analyzer(object) :
        result = elem_tree.xpath(path)[0]
        result_html = tostring(result)
        return result_html
-
+    
+   
    @staticmethod
    def find_path() :
        html_file = "ETOZGianfranco.html"
@@ -39,7 +44,7 @@ class Analyzer(object) :
        result = elem_tree.xpath(path)[0]
        result_html = tostring(result)
        result_class = result.attrib.get('class')
-       print '{0} -> {1}'.format(input_string, elem_tree.getpath(node))
+       print('{0} -> {1}'.format(input_string, elem_tree.getpath(node)))
        #Use template
        html_file2 = "ETOZCocktail.html"
        elem_tree2 = lh.parse(html_file2)
@@ -52,7 +57,8 @@ class Analyzer(object) :
        #Insert record
        dao.insert_record('TestUrl', result_html, 19)
        dao.insert_record('TestUrl', result2_html, 19)
+   
 
-Analyzer.find_path()
-path = Analyzer.search_path('http://www.etoz.ch/kyoto/', 'Gianfranco Frattini')
-Analyzer.search_content('http://www.etoz.ch/ospite/', path)
+#Analyzer.find_path()
+#path = Analyzer.search_path('http://www.etoz.ch/kyoto/', 'Gianfranco Frattini')
+#Analyzer.search_content('http://www.etoz.ch/ospite/', path)
