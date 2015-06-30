eb6129ad6fccea7186cd002861213500e5df5520
Peter Schaab <pejaab@gmail.com>
2015-06-30 16:35:09 +0200

    initializes Crawler as Database Model

--- impudo-ui/interface/models.py
+++ impudo-ui/interface/models.py
@@ -3,17 +3,20 @@ from django.db import models
 
 from interface.analyzer.analyzer import Analyzer
 
-class TemplateItem(models.Model):
+class Template(models.Model):
     url_abbr = models.TextField()
     url = models.URLField()
     desc = models.TextField()
     
-    analyzer = Analyzer()
-
     def __str__(self):
         return self.url_abbr
     
     def get_absolute_url(self):
         return reverse('view_template', args=[self.id])
 
+
+class Crawler(models.Model):
+    template = models.IntegerField(primary_key=True)
+    paths = models.TextField()
+    results = models.TextField()
 
