61292f6caf11920142371ac077f5616ff814a8bd
Peter Schaab <pejaab@gmail.com>
2015-06-30 16:36:54 +0200

    start changes to include crawler in redirect views

--- impudo-ui/interface/views.py
+++ impudo-ui/interface/views.py
@@ -1,36 +1,36 @@
-import re
-
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from interface.forms import TemplateItemForm
-from interface.models import TemplateItem
+from interface.forms import TemplateForm
+from interface.models import Template
 
 def home_page(request):
-    return render(request, 'home.html', {'form': TemplateItemForm()})
+    return render(request, 'home.html', {'form': TemplateForm()})
 
 def view_template(request, template_id):
-    item = TemplateItem.objects.get(id=template_id)
-    form = TemplateItemForm()
+    item = Template.objects.get(id=template_id)
+    form = TemplateForm()
     if request.method == 'POST':
-        form = TemplateItemForm(data=request.POST, instance=item)
+        form = TemplateForm(data=request.POST, instance=item)
         if form.is_valid():
             item = form.save()
+            form.analyze()
             return redirect(item)  
         else:
-            return render(request, 'template.html', {'item': item, 'form': form}) 
+            return render(request, 'template.html', {'form': form, 'item': item}) 
     return render(
             request, 'template.html', {
                 'item': item, 
-                'form': TemplateItemForm(initial={'url': item.url, 'desc': item.desc})
+                'form': TemplateForm(initial={'url': item.url, 'desc': item.desc})
                 }
             )
 
 def new_template(request):
-    form = TemplateItemForm(data=request.POST)
+    form = TemplateForm(data=request.POST)
     if form.is_valid():
         item = form.save()
+        form.analyze()
         return redirect(item)
     else:
         return render(request, 'home.html', {'form': form})
