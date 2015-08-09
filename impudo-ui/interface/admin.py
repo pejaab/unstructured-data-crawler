b29c93637afb5c5d27d4d63f9dac2a5ecb87c9d8
Peter Schaab <pejaab@gmail.com>
2015-08-09 15:48:19 +0200

    makes use of import_export to make export capabilities available

--- impudo-ui/interface/admin.py
+++ impudo-ui/interface/admin.py
@@ -1,18 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-
+from django.db import models
+from django.forms import TextInput, Textarea
 from interface.models import Crawler, Record, Template
-
-#admin.site.unregister(User)
-#admin.site.unregister(Group)
+from import_export import resources
+from import_export.admin import ExportActionModelAdmin
 
 class AdminSite(admin.AdminSite):
     site_header = 'Manage Crawler'
 
+
+class RecordResource(resources.ModelResource):
+
+    class Meta:
+        model = Record
+
+
+class RecordAdmin(ExportActionModelAdmin):
+    list_filter = (
+            ('template_id', admin.RelatedOnlyFieldListFilter),
+            )
+    search_fields = ['title', 'result', 'template_id']
+    list_display = ('title', 'result', 'template_id')
+
+    resource_class = RecordResource
+
+
 class CrawlerInline(admin.TabularInline):
     model = Crawler
     extra = 0
-    exclude = ('url', 'active')
+    exclude = ('url',)
     max_num = 0
     readonly_fields = ('xpath', 'content', 'crawled')
 
@@ -21,18 +38,13 @@ class TemplateAdmin(admin.ModelAdmin):
     readonly_fields = ('url_abbr', 'url', 'desc')
     list_display = ('url_abbr', 'url', 'desc')
 
-    inlines = [CrawlerInline]
+
+    inlines = [CrawlerInline,]
 
     def has_add_permission(self, request):
         return False
 
-#class CrawlerAdmin(admin.ModelAdmin):
-
-#    def has_add_permission(self, request):
-#        return False
 
 admin_site = AdminSite(name='admin')
 admin_site.register(Template, TemplateAdmin)
-admin_site.register(Record)
-#admin.site.register(Template, TemplateAdmin)
-#admin.site.register(Crawler, CrawlerAdmin)
+admin_site.register(Record, RecordAdmin)
