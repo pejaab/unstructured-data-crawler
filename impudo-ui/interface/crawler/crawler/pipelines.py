7bd1fa1e9e5b2c45ddcc8860dc96bfaaff987c58
Peter Schaab <pejaab@gmail.com>
2015-10-30 20:38:14 +0100

    corrects function order and indention

--- impudo-ui/interface/crawler/crawler/pipelines.py
+++ impudo-ui/interface/crawler/crawler/pipelines.py
@@ -10,107 +10,108 @@ from dao import Dao
 import re
 
 class ImpudoImagesPipeline(ImagesPipeline):
+    d = Dao()
 
-	d = Dao()
+    #Name download version
+    def file_path(self, request, response=None, info=None):
+        template_id = request.meta['template_id']
+        image_guid = request.url.split('/')[-1]
+        return 'full/template_{0}/{1}'.format(template_id, image_guid)
+        #return 'full/%s' % (image_guid)
 
-        #Name download version
-        def file_path(self, request, response=None, info=None):
-            template_id = request.meta['template_id']
-            image_guid = request.url.split('/')[-1]
-            return 'full/template_{0}/{1}'.format(template_id, image_guid)
-            #return 'full/%s' % (image_guid)
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url, meta={'template_id': item['template_id']})
 
-	def get_media_requests(self, item, info):
-		for image_url in item['image_urls']:
-                    yield scrapy.Request(image_url, meta={'template_id': item['template_id']})
+    def extract_price_and_dimensions(self, text):
+        #moneyregexold = r"""[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{2})?(?:\.-)?|(?:,[0-9]{3})*(?:\.[0-9]{2})?(?:\.-)?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)?(?:\.-)?"""
+        moneyregex = r"""[0-9]+(?:[,.]?[0-9]+)*(?:\.-)?"""
 
-	def item_completed(self, results, item, info):
-		image_paths = [x['path'] for ok, x in results if ok]
-		item['image_paths'] = image_paths
+        #fullwordonly1= r"""(?:^|\s)"""
+        #fullwordonly2= r"""(?:$|\s)"""
+        #example usage:
+        #moneymatch = "(?:"+fullwordonly1+currenciespattern+moneyregex+fullwordonly2+")|(?:"+fullwordonly1+moneyregex+currenciespattern+fullwordonly2+")"
 
-		self.save_item_in_db(item)
-		
-		return item
+        currenciespattern = r"""(?:€|\$|£|EUR|USD|Euro|Dollar|DKK|GBP|AUD|AU\$|CAD|HKD|MXN|CHF|DKK|NOK|Fr\.|¥|JPY|₽|RUB|CNY)"""
+        #moneymatch = "(?:"+currenciespattern+ r"[ \t]?"+moneyregex+ r")|(?:" +moneyregex+ r"[ \t]?" +currenciespattern+ ")"
+        moneymatch = "(?:" +currenciespattern+ r"[ \t]?"+moneyregex+ r")|(?:(?:\s)" +moneyregex+ r"[ \t]?" +currenciespattern+ ")"
 
-	def extract_price_and_dimensions(self, text):
+        #to match dimensions in text
+        lnum = r"""(?:[0-9]*\,[0-9]+|[0-9]*\.[0-9]+|[0-9]+)"""
+        #shorthand for Depth, Width, Height, Hoehe, Lange, Tiefe, Breite
+        lwh = r"""(?:d|D|w|W|h|H|T|t|L|B)"""
+        lunit = r"""(?:mm|cm|dm)"""
+        lunit2 = r"""(?:"|’’|'')"""
 
-		#moneyregexold = r"""[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{2})?(?:\.-)?|(?:,[0-9]{3})*(?:\.[0-9]{2})?(?:\.-)?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)?(?:\.-)?"""
-		moneyregex = r"""[0-9]+(?:[,.]?[0-9]+)*(?:\.-)?"""
-		
-		#fullwordonly1= r"""(?:^|\s)"""
-		#fullwordonly2= r"""(?:$|\s)"""
-		#example usage:
-		#moneymatch = "(?:"+fullwordonly1+currenciespattern+moneyregex+fullwordonly2+")|(?:"+fullwordonly1+moneyregex+currenciespattern+fullwordonly2+")"
-		
-		currenciespattern = r"""(?:€|\$|£|EUR|USD|Euro|Dollar|DKK|GBP|AUD|AU\$|CAD|HKD|MXN|CHF|DKK|NOK|Fr\.|¥|JPY|₽|RUB|CNY)"""
-		#moneymatch = "(?:"+currenciespattern+ r"[ \t]?"+moneyregex+ r")|(?:" +moneyregex+ r"[ \t]?" +currenciespattern+ ")"
-		moneymatch = "(?:" +currenciespattern+ r"[ \t]?"+moneyregex+ r")|(?:(?:\s)" +moneyregex+ r"[ \t]?" +currenciespattern+ ")"
+        #dimmatch = "("+lnum + r"[ \t]?x[ \t]?"+lnum  + r"(?:[ \t]?x[ \t]?" +lnum+ r")?" +"|"+lnum+ r"[ \t]*"+lwh+r"*[ \t]*"+lunit+"|"+lnum+lwh+"|"+r"\b"+lwh+ r"(\:|\.)?[ \t]*"+lnum+ r"[ \t]?"+lunit+"?" +")"
+        dimmatch = "("+lnum +lwh+"?" +r"[ \t]?" +lunit+"?" + r"[ \t]?x[ \t]?"+lnum +lwh+"?"+r"[ \t]?" +lunit+"?" + r"(?:[ \t]?x[ \t]?" +lnum +lwh+"?"+r"[ \t]?" +lunit+"?" + r")?" +"|"+lnum+ r"[ \t]?"+lwh+r"?[ \t]?"+lunit+"|"+r"\b"+lwh+ r"(\:|\.)[ \t]*"+lnum+ r"[ \t]?"+lunit +"?"+"|"+r"\b"+lnum+r"[ \t]?"+lunit2+")"
 
-		#to match dimensions in text
-		lnum = r"""(?:[0-9]*\,[0-9]+|[0-9]*\.[0-9]+|[0-9]+)"""
-		#shorthand for Depth, Width, Height, Hoehe, Lange, Tiefe, Breite
-		lwh = r"""(?:d|D|w|W|h|H|T|t|L|B)"""
-		lunit = r"""(?:mm|cm|dm)"""
-		lunit2 = r"""(?:"|’’|'')"""
+        #remove quotation marks
+        mtext = text.replace("\'","").replace("’","").replace("`","").replace("´","")
 
-		#dimmatch = "("+lnum + r"[ \t]?x[ \t]?"+lnum  + r"(?:[ \t]?x[ \t]?" +lnum+ r")?" +"|"+lnum+ r"[ \t]*"+lwh+r"*[ \t]*"+lunit+"|"+lnum+lwh+"|"+r"\b"+lwh+ r"(\:|\.)?[ \t]*"+lnum+ r"[ \t]?"+lunit+"?" +")" 
-		dimmatch = "("+lnum +lwh+"?" +r"[ \t]?" +lunit+"?" + r"[ \t]?x[ \t]?"+lnum +lwh+"?"+r"[ \t]?" +lunit+"?" + r"(?:[ \t]?x[ \t]?" +lnum +lwh+"?"+r"[ \t]?" +lunit+"?" + r")?" +"|"+lnum+ r"[ \t]?"+lwh+r"?[ \t]?"+lunit+"|"+r"\b"+lwh+ r"(\:|\.)[ \t]*"+lnum+ r"[ \t]?"+lunit +"?"+"|"+r"\b"+lnum+r"[ \t]?"+lunit2+")" 
+        moneymatches = []
 
-		#remove quotation marks
-		mtext = text.replace("\'","").replace("’","").replace("`","").replace("´","")
-		
-		moneymatches = []
+        for r in re.findall(moneymatch,mtext,re.IGNORECASE):
+            if r:
+                moneymatches.append(r)
 
-		for r in re.findall(moneymatch,mtext,re.IGNORECASE):
-			if r:
-				moneymatches.append(r)
+        dimensionmatches = []
 
-		dimensionmatches = []
+        for r in re.findall(dimmatch,text,re.IGNORECASE):
+            if r[0]:
+                dimensionmatches.append(r[0])
 
-		for r in re.findall(dimmatch,text,re.IGNORECASE):
-			if r[0]:
-				dimensionmatches.append(r[0])
+        return (moneymatches,dimensionmatches)
 
 
-		return (moneymatches,dimensionmatches)
+
+    def extract_and_save_details(self, title, content):
+        #extract from content
+        x = self.extract_price_and_dimensions(content)
+
+        foundprices = ""
+        for a in x[0]:
+            foundprices += "".join(a.split()) + " "
+        foundprices = foundprices.strip()
+
+        founddims = ""
+        for a in x[1]:
+            founddims += "".join(a.split()) + " "
+        founddims = founddims.strip()
+
+        #extract from title
+        x = self.extract_price_and_dimensions(title)
+        for a in x[0]:
+            foundprices += "".join(a.split()) + " "
+        foundprices = foundprices.strip()
+        for a in x[1]:
+            founddims += "".join(a.split()) + " "
+        founddims = founddims.strip()
+
+        return foundprices, founddims
+
+    def save_item_in_db(self, item):
+        #insert record
+        foundprices, founddims = self.extract_and_save_details(item['title'],item['content'])
+
+        self.d.insert_record(item['title'], item['url'], item['content'], item['template_id'], foundprices, founddims)
+        record_id = self.d.get_last_insert_id()[0]
+
+        image_urls = item['image_urls']
+        image_paths = item['image_paths']
+        #insert pictures
+        for img in image_paths:
+            self.d.insert_image(img, record_id)
 
 
-	def extract_and_save_details(self, id, title, content):
-		#extract from content
-		x = self.extract_price_and_dimensions(content)
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        item['image_paths'] = image_paths
 
-		foundprices = ""
-		for a in x[0]:
-			foundprices += "".join(a.split()) + " " 
-		foundprices = foundprices.strip()
+        self.save_item_in_db(item)
 
-		founddims = ""
-		for a in x[1]:
-			founddims += "".join(a.split()) + " " 
-		founddims = founddims.strip()
+        return item
 
-		#extract from title
-		x = self.extract_price_and_dimensions(title)
-		for a in x[0]:
-			foundprices += "".join(a.split()) + " " 
-		foundprices = foundprices.strip()
-		for a in x[1]:
-			founddims += "".join(a.split()) + " " 
-		founddims = founddims.strip()
-                
-                return foundprices, founddims
 
-	def save_item_in_db(self, item):
-		#insert record
-		record_id = self.d.get_last_insert_id()[0]
 
-		image_urls = item['image_urls']
-		image_paths = item['image_paths']
-		#insert pictures
-		for img in image_paths:
-			self.d.insert_image(img, record_id)
 
-		#extract and save details (prices, dimensions, ...)
-		foundprices, founddims = self.extract_and_save_details(record_id,item['title'],item['content'])
-		
-		self.d.insert_record(item['title'], item['url'], item['content'], item['template_id'], foundprices, founddims)
